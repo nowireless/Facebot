b719854f860fb8e3ad380e5c4f88757c6b6cf4b6
benpankow <pankow.ben@gmail.com>
2016-03-25 14:05:21 -0500

    Finished up bot framework, now ready for the face swap

--- facebot.py
+++ facebot.py
@@ -1,17 +1,31 @@
 from config import *
 import telepot
 import time
+from shutil import copyfile
 
 FILE_QUALITY = 2 - 1
+DL_PATH = "temp/dl.jpg"
+OUTPUT_PATH = "temp/output.jpg"
 
-def addDownload(fileid, chatid):
-	print fileid, chatid
-	return # TODO
+def process(fileid, chatid):
+
+	bot.downloadFile(fileid, DL_PATH)
+
+	# TODO PROCESS HERE
+	copyfile(DL_PATH, OUTPUT_PATH)
+
+	pic = open(OUTPUT_PATH, "rb")
+	bot.sendPhoto(chatid, pic)
+
+
 
 # Handle all messages
 def handle(msg):
-	if "photo" in msg:
-			addDownload(msg["photo"][FILE_QUALITY]["file_id"], msg["chat"]["id"])
+	try:
+		if "photo" in msg:
+			process(msg["photo"][FILE_QUALITY]["file_id"], msg["chat"]["id"])
+	except e:
+		print "ERROR! " + e
 
 bot = telepot.Bot(key)
 
@@ -19,4 +33,4 @@ bot.notifyOnMessage(handle)
 print "Running..."
 
 while 1:
-    time.sleep(30)
+	time.sleep(30)
