81a7b99b4f385485f5497c2bd8c2b65062ffbceb
benpankow <pankow.ben@gmail.com>
2016-03-25 13:39:22 -0500

    Added framework for downloading files

--- facebot.py
+++ facebot.py
@@ -2,9 +2,16 @@ from config import *
 import telepot
 import time
 
+FILE_QUALITY = 2 - 1
+
+def addDownload(fileid, chatid):
+	print fileid, chatid
+	return # TODO
+
 # Handle all messages
 def handle(msg):
-    print msg
+	if "photo" in msg:
+			addDownload(msg["photo"][FILE_QUALITY]["file_id"], msg["chat"]["id"])
 
 bot = telepot.Bot(key)
 
